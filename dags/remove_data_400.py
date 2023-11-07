112f9c46d031a77caa8567951f3c2f4022113f8b
hooniegit <neivekim76@gmail.com>
2023-11-07 22:25:46 +0000

    231108  🧑‍🏭 feat & test: remove data 로직 구현, 테스트 및 버그 수정

--- dags/remove_data_400.py
+++ dags/remove_data_400.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from airflow.models import Variable
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import BranchPythonOperator
 
 
 # get variables
@@ -12,7 +13,6 @@ sensor_id = 400
 
 # set execution date & time
 date = "{{ (execution_date + macros.timedelta(days=1)).strftime('%Y-%m-%d') }}"
-time = "{{ (execution_date - macros.timedelta(hours=1)).strftime('%H') }}"
 
 # default argument 설정
 default_args = {
@@ -37,21 +37,59 @@ start = EmptyOperator(
 	dag=dag
 )
 
+
+# curl
+curl_check_data = BashOperator(
+    task_id="curl.check.data",
+    bash_command=f"curl 'http://{host_fastapi}:{port_fastapi}/check/{sensor_id}?date={date}'",
+    dag=dag
+)
+
+
+def define_way(**kwargs):
+    return_value = kwargs['task_instance'].xcom_pull(task_ids='curl.check.data', key='return_value')
+    print(f">>>>>>> RETURN VALUE : {return_value}")
+
+    if return_value == "PASS":
+        return 'curl.remove.data'
+    elif return_value == "FAIL":
+        return 'send.noti.check'
+    
+
+branch_define_way = BranchPythonOperator(
+    task_id='branch.define.way',
+    provide_context=True,
+    python_callable=define_way,
+    dag=dag,
+)
+
+
 # curl
 curl_remove_data = BashOperator(
     task_id="curl.remove.data",
-    bash_command=f"curl 'http://{host_fastapi}:{port_fastapi}/parquet/local/{sensor_id}?date={date}&time={time}'",
+    bash_command=f"curl 'http://{host_fastapi}:{port_fastapi}/remove/{sensor_id}?date={date}'",
     dag=dag
 )
 
 # send LINE notification
-send_noti = BashOperator(
-    task_id='send.noti',
-    bash_command='''
-    curl -X POST -H 'Authorization: Bearer fxANtArqOzDWxjissz34JryOGhwONGhC1uMN8qc59Z3'
-                 -F '<MESSAGE>' 
-                 https://notify-api.line.me/api/notify
-    ''',
+send_noti_check = BashOperator(
+    task_id='send.noti.check',
+    bash_command=f"""
+    curl -X POST -H 'Authorization: Bearer jvQDUjm6vGgBqG1aA3Sm7rIhhHENN2PLP68CwRqjrMl' \
+    -F 'message= {date} 일자 {sensor_id}번 센서 데이터: 누적 개수 부족' \
+    https://notify-api.line.me/api/notify
+    """,
+    dag=dag
+)
+
+# send LINE notification
+send_noti_error = BashOperator(
+    task_id='send.noti.error',
+    bash_command=f"""
+    curl -X POST -H 'Authorization: Bearer jvQDUjm6vGgBqG1aA3Sm7rIhhHENN2PLP68CwRqjrMl' \
+    -F 'message= {date} 일자 {sensor_id}번 센서 데이터: 삭제 과정에서 오류 발생' \
+    https://notify-api.line.me/api/notify
+    """,
     dag=dag,
 	trigger_rule='one_failed'
 )
@@ -65,4 +103,6 @@ finish = EmptyOperator(
 
 
 # process
-start >> curl_remove_data >> [send_noti, finish]
+start >> curl_check_data >> branch_define_way >> [curl_remove_data, send_noti_check]
+send_noti_check >> finish
+curl_remove_data >> [send_noti_error, finish]
